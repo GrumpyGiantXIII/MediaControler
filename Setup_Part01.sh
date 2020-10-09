fcbe443f58116a51698aaed11b7d112fd618b022
Grumpy Giant <grumpy@iswleuven.be>
2020-10-09 10:31:46 +0200

    version 0.1.13

--- Setup_Part01.sh
+++ Setup_Part01.sh
@@ -1,24 +1,26 @@
-ExcpectedLocation="usr/lib/x86_64-linux-gnu/vlc/lua/playlist/youtube.luac"
-LocationOnSystem=dpkg -L vlc-plugin-base | grep youtube.lua
+ExcpectedLocation="/usr/lib/x86_64-linux-gnu/vlc/lua/playlist/youtube.luac"
+LocationOnSystem=$(dpkg -L vlc-plugin-base | grep youtube.lua)
 
 sudo apt update
 sudo apt upgrade
 sudo apt install vlc -y
 sudo apt install python3 -y
 sudo apt install python3-pip -y
-mkdir temp
-wget https://raw.githubusercontent.com/videolan/vlc/master/share/lua/playlist/youtube.lua -P temp
-if[[$ExcpectedLocation == $LocationOnSystem]] then
-	sudo rm /usr/lib/x86_64-linux-gnu/vlc/lua/playlist/youtube.luac
-	sudo cp temp/youtube.lua /usr/lib/x86_64-linux-gnu/vlc/lua/playlist/youtube.lua
+mkdir ~/temp
+wget https://raw.githubusercontent.com/videolan/vlc/master/share/lua/playlist/youtube.lua -P ~/temp
+
+if [ "$ExcpectedLocation" == "$LocationOnSystem" ]; then
+	sudo rm "$ExcpectedLocation"
+	sudo cp ~/temp/youtube.lua /usr/lib/x86_64-linux-gnu/vlc/lua/playlist/youtube.lua
 else
 	echo "Error cant continue unexpected location for youtube.lua"
 fi
 
 sudo systemctl set-default multi-user
 echo "rebooting in 30 second's to cance cance script and reboot manualy"
+echo "after reboot start script 2"
 sleep 30
-reboot
+sudo reboot
 
 
 
--- test.sh
+++ test.sh
@@ -1,1 +1,14 @@
-test of git
+LocationOnSystem=$(dpkg -L vlc-plugin-base | grep youtube.lua)
+ExcpectedLocation="/usr/lib/x86_64-linux-gnu/vlc/lua/playlist/youtube.luac"
+
+
+echo $LocationOnSystem
+echo "expected location"
+echo $ExcpectedLocation
+
+if [ "$LocationOnSystem" == "$ExcpectedLocation" ]; then
+	echo "succes sucker"
+else
+	echo "we failed"
+fi
+echo "end"
