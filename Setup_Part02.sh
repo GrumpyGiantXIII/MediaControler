fed7045a5ec1d786064d5cc69548dfb1156eba89
GrumpyGiant <grumpy@iswleuven.be>
2020-10-09 13:39:54 +0200

    Version 0.2.0.2

--- Setup_Part02.sh
+++ Setup_Part02.sh
@@ -5,7 +5,7 @@ sudo mkdir /mnt/VLCMediaController/LaunchScripts
 sudo cp Shell/LaunchScripts/* /mnt/VLCMediaController/LaunchScripts
 sudo chmod 0755 /mnt/VLCMediaController/LaunchScripts/*
 
-Sudo cp Shell/Services/MediaServer_StartStopService.service /etc/systemd/system/MediaServer_StartStopService.service
+sudo cp Shell/Services/MediaServer_StartStopService.service /etc/systemd/system/MediaServer_StartStopService.service
 sudo cp Shell/Services/SoundPannelServer_StartStopService.service /etc/systemd/system/SoundPannelServer_StartStopService.service
 sudo systemctl enable MediaServer_StartStopService.service 
 sudo systemctl enable SoundPannelServer_StartStopService.service
