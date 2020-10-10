87cfaef699b751aeba332fe0d731fdcc4f17fc03
GrumpyGiant <grumpy@iswleuven.be>
2020-10-10 22:00:50 +0200

    Version 0.3.0.1

--- Setup_Part01.sh
+++ Setup_Part01.sh
@@ -32,13 +32,12 @@ sudo chmod 0755 /mnt/VLCMediaController/LaunchScripts/LaunchSoundPannelServer.sh
 
 sudo cp Shell/PulseAudio_Config/client.conf /etc/pulse/client.conf
 
-sudo adduser ragnarok pulse-acces
+sudo adduser ragnarok pulse-access
 sudo adduser root pulse-acces
 
 sudo cp Shell/Services/PulseAudio.service /etc/systemd/system/PulseAudio.service
 sudo cp Shell/Services/MediaServer_StartStopService.service /etc/systemd/system/MediaServer_StartStopService.service
-sudo cp Shell/Services/SoundP
-annelServer_StartStopService.service /etc/systemd/system/SoundPannelServer_StartStopService.service
+sudo cp Shell/Services/SoundPannelServer_StartStopService.service /etc/systemd/system/SoundPannelServer_StartStopService.service
 sudo systemctl enable PulseAudi.service
 sudo systemctl enable MediaServer_StartStopService.service
 sudo systemctl enable SoundPannelServer_StartStopService.service
